7d824ad254c60a69006a5e2c5b35fba6957222ef
ScottyJ59 <scott.c.james@comcast.net>
2021-03-13 18:45:19 -0600

    added comments

--- object_detection.py
+++ object_detection.py
@@ -4,10 +4,10 @@ import argparse
 import time
 
 def load_yolo():
-    net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
+    net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg") #customize weights
     classes = []
     with open("coco.names", "r") as f:
-        classes = [line.strip() for line in f.readlines()]
+        classes = [line.strip() for line in f.readlines()] #customize classes
     layers_names = net.getLayerNames()
     output_layers = [layers_names[i[0]-1] for i in net.getUnconnectedOutLayers()]
     colors = np.random.uniform(0, 255, size=(len(classes), 3))
