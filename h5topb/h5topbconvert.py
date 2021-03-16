9efb1d4e9e5002d051ff279924604e5ec4daf641
ScottyJ59 <scott.c.james@comcast.net>
2021-03-16 10:21:05 -0500

    updated files

---
+++ h5topb/h5topbconvert.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+
+#model = tf.keras.models.load_model(sidewalk_weights.h5)
+#tf.saved_model.save(model, sidewalk_weights.pb)
+
+
+pre_model = tf.keras.models.load_model(sidewalk_weights.h5)
+pre_model.save(sidewalk_weights)
