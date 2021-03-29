bd2ce58c467acb11fa77d15c94d1786e91341774
Max Asselmeier <35544813+ma53ma@users.noreply.github.com>
2021-03-29 12:25:19 -0500

    Delete h5topb directory

--- h5topb/h5topbconvert.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-
-#model = tf.keras.models.load_model(sidewalk_weights.h5)
-#tf.saved_model.save(model, sidewalk_weights.pb)
-
-
-pre_model = tf.keras.models.load_model("sidewalk_weights2.h5")
-pre_model.save("sidewalk_weights2")
